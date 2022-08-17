b627e3a79477635904ca8e82af923fc5c3bb3a65
Eduardo Canova <eduardocanova23@gmail.com>
2022-08-16 21:40:21 -0300

    First steps, store initial data from users

--- Stock Quote Alert/Program.cs
+++ Stock Quote Alert/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace StockQuoteAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-Vindo ao monitoramento de ativos Stock Quote Alert");
+            Console.WriteLine("------------------------------------------------------");
+
+            // Primeira interação com o usuário, pedir os dados e armazená-los
+            Console.WriteLine("Digite o código do ativo a ser monitorado, o preço de referência para venda e o preço de referência para compra");
+
+            string parameters = Console.ReadLine();
+
+            string[] parameters_list = parameters.Split(" ");
+
+            string ativ = parameters_list[0];
+            double min = double.Parse(parameters_list[1], CultureInfo.InvariantCulture);
+            double max = double.Parse(parameters_list[2], CultureInfo.InvariantCulture);
+        }
+    }
+}
